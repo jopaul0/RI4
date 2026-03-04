1bc3e6620093df5f843a6afab435c0678feb18a6
João Paulo <jopaulo.as8@gmail.com>
2026-03-04 08:56:12 -0300

    fix: corrigindo encapsulamento

--- models/Cliente.js
+++ models/Cliente.js
@@ -8,15 +8,7 @@ export default class Cliente {
         this.endereco = endereco;
     }
 
-    // --- GETTERS ---
-    get nome() { return this.nome; }
     get cpf() { return this.#cpf; }
-    get telefones() { return this.telefones; }
-    get endereco() { return this.endereco; }
-
-    // --- SETTERS ---
-    set nome(valor) { this.nome = valor; }
-    set endereco(valor) { this.endereco = valor; }
 
     getNomeUpper() { return this.nome.toUpperCase(); }
     getNomeLower() { return this.nome.toLowerCase(); }
