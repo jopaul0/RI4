f5d019773ccbf3c61cc9e5b895b130fa13916614
João Paulo <jopaulo.as8@gmail.com>
2026-03-04 08:41:25 -0300

    feat: adiciona a classe de endereços

---
+++ models/Endereco.js
@@ -0,0 +1,8 @@
+export default class Endereco {
+    constructor(estado, cidade, rua, numero) {
+        this.estado = estado;
+        this.cidade = cidade;
+        this.rua = rua;
+        this.numero = numero;
+    }
+}
