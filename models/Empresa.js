9a5a5ad90c2a9ee469f61f6687269cd0fba90fc5
João Paulo <jopaulo.as8@gmail.com>
2026-03-04 08:43:41 -0300

    feat: adiciona a classe de empresas

---
+++ models/Empresa.js
@@ -0,0 +1,11 @@
+export default class Empresa {
+    #cnpj;
+    constructor(razaoSocial, nomeFantasia, cnpj, endereco) {
+        this.razaoSocial = razaoSocial;
+        this.nomeFantasia = nomeFantasia;
+        this.#cnpj = cnpj;
+        this.endereco = endereco;
+        this.clientes = new Set();
+        this.telefones = new Set();
+    }
+}
