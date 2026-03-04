b31066f5d59515e5e92a6a5ab4ee722eed367770
João Paulo <jopaulo.as8@gmail.com>
2026-03-04 08:48:06 -0300

    feat: métodos de caixa baixa e alta em Endereço

--- models/Endereco.js
+++ models/Endereco.js
@@ -5,4 +5,13 @@ export default class Endereco {
         this.rua = rua;
         this.numero = numero;
     }
+
+    getEstadoUpper() { return this.estado.toUpperCase(); }
+    getEstadoLower() { return this.estado.toLowerCase(); }
+
+    getCidadeUpper() { return this.cidade.toUpperCase(); }
+    getCidadeLower() { return this.cidade.toLowerCase(); }
+
+    getRuaUpper() { return this.rua.toUpperCase(); }
+    getRuaLower() { return this.rua.toLowerCase(); }
 }
